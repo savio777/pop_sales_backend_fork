1c0f1dad390254a43b6115aedb22eb8086a9e7a0
Gabriel Andrade <138128244+MendoncaGabriel@users.noreply.github.com>
2025-03-01 18:55:33 -0300

    feat: add zod and dotenv, create lib to envs and prisma

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,17 @@
-import { PrismaClient } from '@prisma/client'
 import { app } from './app'
-
-const prisma = new PrismaClient()
+import { env } from './lib/env'
 
 app.get('/', async (request, reply) => {
   return { message: 'Olá, Fastify com Prisma!' }
 })
 
-app.get('/usuarios', async (request, reply) => {
-  const usuarios = await prisma.usuario.findMany()
-  return usuarios
-})
-
-app.listen({ port: 3000, host: '0.0.0.0' }, (err, address) => {
-  if (err) {
-    console.error(err)
-    process.exit(1)
+app.listen(
+  { port: env.PORT, host: env.HOST },
+  (err, address) => {
+    if (err) {
+      console.error(err)
+      process.exit(1)
+    }
+    console.log(`Servidor rodando em ${address}`)
   }
-  console.log(`Servidor rodando em ${address}`)
-})
+)
