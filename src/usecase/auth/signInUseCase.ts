3043596a549ae75b1aeb9526992d3e750edebb00
Gabriel Andrade <138128244+MendoncaGabriel@users.noreply.github.com>
2025-03-03 11:51:01 -0300

    feat: create use case to sign in

---
+++ src/usecase/auth/signInUseCase.ts
@@ -0,0 +1,45 @@
+import { NotFoundError } from "@/error/notfound.error";
+import { UnauthorizedError } from "@/error/unauthorized.error";
+import { env } from "@/lib/env";
+import { UserRepository } from "@/repository/userRepository";
+import bcrypt from 'bcrypt';
+import jwt from "jsonwebtoken"
+
+interface SignInInputs {
+  email: string,
+  password: string
+}
+
+export class SignUpUseCase {
+  constructor(
+    private readonly userRepository: UserRepository
+  ) {}
+
+  async execute(data: SignInInputs) {
+    const user = await this.userRepository.getByEmail(data.email)
+    if(!user){
+      throw new NotFoundError("user not found with e-mail")
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(data.password, user.password);
+    if (!isPasswordCorrect) {
+      throw new UnauthorizedError("e-mail or password are not correct")
+    }
+
+    const payload = {
+      userId: user.id
+    }
+
+    const signOptions = {
+      expiresIn: 60 * 60 * 24 * 7 // 7 days
+    }
+
+    const token = jwt.sign(
+      payload,
+      env.PRIVATE_KEY, 
+      signOptions 
+    );
+    
+    return {user, token}
+  }
+}
