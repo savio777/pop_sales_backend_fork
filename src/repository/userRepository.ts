717ac75e6fadb54098e52f97a50224346ed21567
Gabriel Andrade <138128244+MendoncaGabriel@users.noreply.github.com>
2025-03-03 10:28:32 -0300

    feat: create user prisma repository

--- src/repository/userRepository.ts
+++ src/repository/userRepository.ts
@@ -2,8 +2,8 @@ import { Prisma, User } from "@prisma/client";
 
 export interface UserRepository {
   create(data: Prisma.UserCreateInput): Promise<User>
-  getById(id: String): Promise<User | null>
-  getByEmail(email: String): Promise<User | null>
+  getById(id: string): Promise<User | null>
+  getByEmail(email: string): Promise<User | null>
   update({id, data}:{id: string, data: Prisma.UserUpdateInput}): Promise<User | null>
-  delete(id: String): Promise<void>
+  delete(id: string): Promise<void>
 }
