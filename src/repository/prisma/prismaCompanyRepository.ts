31ad1c2b26a93ed7729f1caff07d6cc5439a8098
Gabriel Andrade <138128244+MendoncaGabriel@users.noreply.github.com>
2025-03-05 19:36:34 -0300

    feat: create prisma company repository

---
+++ src/repository/prisma/prismaCompanyRepository.ts
@@ -0,0 +1,36 @@
+import { Prisma, Company } from "@prisma/client";
+import { CompanyRepository } from "../companyRepository";
+import { db } from "@/lib/prisma";
+
+export class PrismaCompanyRepository implements CompanyRepository {
+  async create(data: Prisma.CompanyCreateInput): Promise<Company> {
+    const company = await db.company.create({
+      data
+    })
+    return company
+  }
+  async getById(id: string): Promise<Company | null> {
+   const company = await db.company.findUnique({
+      where: {
+        id
+      }
+   })
+   return company
+  }
+  async update({ id, data }: { id: string; data: Prisma.CompanyUpdateInput; }): Promise<Company | null> {
+    const company = await db.company.update({
+      where: {
+        id
+      },
+      data
+    })
+    return company
+  }
+  async delete(id: string): Promise<void> {
+    await db.company.delete({
+      where: {
+        id
+      }
+    })
+  }
+}
