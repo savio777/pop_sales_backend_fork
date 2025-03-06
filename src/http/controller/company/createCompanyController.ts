0f11c4ce21531d22f12a37e750b7bdfe3e7dcb4b
Gabriel Andrade <138128244+MendoncaGabriel@users.noreply.github.com>
2025-03-06 01:42:30 -0300

    fix: correction create company controller

--- src/http/controller/company/createCompanyController.ts
+++ src/http/controller/company/createCompanyController.ts
@@ -1,4 +1,6 @@
+import { BadRequestError } from "@/error/badRequest.error";
 import { PrismaCompanyRepository } from "@/repository/prisma/prismaCompanyRepository";
+import { PrismaUserRepository } from "@/repository/prisma/prismaUserRepository";
 import { CreateCompanyUseCase } from "@/usecase/company/createCompanyUseCase";
 import { FastifyRequest } from "fastify";
 import { FastifyReply } from "fastify/types/reply";
@@ -7,6 +9,11 @@ import { z } from "zod";
 export class CreateCompanyController {
   async handle(req: FastifyRequest, res: FastifyReply){
 
+    if (!req.userAuth?.id) {
+      throw new BadRequestError("userId not informed");
+    }
+    const userId = req.userAuth.id
+    
     const createCompanySchema = z.object({
       name: z.string()
     })
@@ -14,9 +21,14 @@ export class CreateCompanyController {
     const {name} = createCompanySchema.parse(req.body)
 
     const companyRepository = new PrismaCompanyRepository()
-    const createCompanyUseCase = new CreateCompanyUseCase(companyRepository)
+    const userCompany = new PrismaUserRepository()
 
-    await createCompanyUseCase.execute({name})
+    const createCompanyUseCase = new CreateCompanyUseCase(
+      companyRepository,
+      userCompany
+    )
+
+    await createCompanyUseCase.execute({name, userId})
 
     return res.status(200).send()
   }
