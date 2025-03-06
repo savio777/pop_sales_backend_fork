31d59a27c8101230aad880d6a57c46da3549df87
Gabriel Andrade <138128244+MendoncaGabriel@users.noreply.github.com>
2025-03-06 01:19:43 -0300

    fix: remove seed not used

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,10 +3,33 @@ import {db} from "../src/lib/prisma"
 async function main() {
   // Criando permissões básicas
   const permissions = [
-    { name: 'Admin', permissions: ['all:all:all'] },
-    { name: 'DeliveryPerson', permissions: ['get:routes:view', 'post:deliveries:create'] },
-    { name: 'Salesperson', permissions: ['post:sales:create', 'get:customers:view'] },
-    { name: 'Promoter', permissions: ['get:products:view', 'post:brands:promote'] },
+    { 
+      name: 'Admin', 
+      permissions: [
+        'create.company'
+      ]
+    },
+    // { 
+    //   name: 'DeliveryPerson',
+    //   permissions: [
+    //     'get:routes:view',
+    //     'post:deliveries:create'
+    //   ]
+    // },
+    // { 
+    //   name: 'Salesperson', 
+    //   permissions: [
+    //     'post:sales:create', 
+    //     'get:customers:view'
+    //   ]
+    // },
+    // { 
+    //   name: 'Promoter', 
+    //   permissions: [
+    //     'get:products:view',
+    //     'post:brands:promote'
+    //   ]
+    // },
   ];  
   
   for (const perm of permissions) {
