c96e9b25a43f26c3a53aa54cfaf35046a2947b58
Gabriel Andrade <138128244+MendoncaGabriel@users.noreply.github.com>
2025-03-08 14:34:56 -0300

    feat: remove user module usecase

---
+++ src/usecase/userModule/removeUserModuleUseCase copy.ts
@@ -0,0 +1,29 @@
+import { BadRequestError } from "@/error/badRequest.error";
+import { ModuleRepository } from "@/repository/moduleRepository";
+import { UserModuleRepository } from "@/repository/userModuleRepository";
+import { UserRepository } from "@/repository/userRepository";
+
+export class RemoveUserModuleUseCase {
+  constructor(
+    private readonly userModuleRepository: UserModuleRepository,
+    private readonly userRepository: UserRepository,
+    private readonly moduleRepository: ModuleRepository
+  ){}
+
+  async execute(
+    {userId, moduleId}:
+    {userId: string, moduleId: string}
+  ){
+    const user = await this.userRepository.getById(userId)
+    if(!user){
+      throw new BadRequestError("user not exist")
+    }
+
+    const module = await this.moduleRepository.getById(moduleId)
+    if(!module){
+      throw new BadRequestError("module not exist")
+    }
+
+    await this.userModuleRepository.remove({userId, moduleId})
+  }
+}
