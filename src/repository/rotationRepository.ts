5e2c2455b97952fb5dea9108fcb775be61ed2c07
Gabriel Andrade <138128244+MendoncaGabriel@users.noreply.github.com>
2025-03-10 22:23:29 -0400

    feat: add method getById in inteface rotation repository

--- src/repository/rotationRepository.ts
+++ src/repository/rotationRepository.ts
@@ -2,6 +2,7 @@ import { Prisma, Rotation } from "@prisma/client";
 
 export interface RotationRepository {
   create(data: Prisma.RotationCreateInput): Promise<Rotation>
+  getById(id: string): Promise<Rotation | null>
   listByCreatedById(createdById: string): Promise<Rotation[] | null>
   listByAssignedToId(assignedToId: string): Promise<Rotation[] | null>
   update(data: Prisma.RotationUpdateInput): Promise<Rotation>
