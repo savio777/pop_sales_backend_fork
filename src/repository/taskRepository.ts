c4c7d2584e9e81bf2f4ae3bc13d81d966e370933
Gabriel Andrade <138128244+MendoncaGabriel@users.noreply.github.com>
2025-03-11 16:56:31 -0400

    feat(repository): create interface

---
+++ src/repository/taskRepository.ts
@@ -0,0 +1,9 @@
+import { Prisma, Task } from "@prisma/client";
+
+export interface TaskRepository {
+  create(data: Prisma.TaskCreateInput): Promise<Task>
+  delete(id: string): Promise<void>
+  listByUserAssigned(userAssignedId: string): Promise<Task[] | null>
+  listByCompany(companyId: string): Promise<Task[] | null>
+  listByUserCreated(UserCreatedId: string): Promise<Task[] | null>
+}
