f33d9503962ab872edbbbf581c9c1b3f57df408d
Gabriel Andrade <138128244+MendoncaGabriel@users.noreply.github.com>
2025-03-11 14:26:00 -0400

    fix(usecase): DeleteRotationUseCase

--- src/http/controller/rotation/deleteRotationController.ts
+++ src/http/controller/rotation/deleteRotationController.ts
@@ -1,21 +1,33 @@
+import { BadRequestError } from "@/error/badRequest.error";
 import { PrismaRotationRepository } from "@/repository/prisma/prismaRotationRepository";
+import { PrismaUserRepository } from "@/repository/prisma/prismaUserRepository";
 import { DeleteRotationUseCase } from "@/usecase/rotation/deleteRotationUseCase";
 import { FastifyReply, FastifyRequest } from "fastify";
 import { z } from "zod";
 
 export class DeleteRotationController {
   async handle(req: FastifyRequest, res: FastifyReply){
+    if (!req.userAuth?.id) {
+      throw new BadRequestError("userId not informed");
+    }
+    const userId = req.userAuth.id
+
     const deleteRotationRequestParams = z.object({
       rotationId: z.string().uuid()
     })
     const {rotationId} = deleteRotationRequestParams.parse(req.params)
 
     const rotationRepository = new PrismaRotationRepository()
+    const userRepository = new PrismaUserRepository()
     const deleteRotationUseCase = new DeleteRotationUseCase(
-      rotationRepository
+      rotationRepository,
+      userRepository
     )
 
-    await deleteRotationUseCase.execute(rotationId)
+    await deleteRotationUseCase.execute({
+      rotationId,
+      userId
+    })
 
     return res.status(200).send()
   }
--- src/usecase/rotation/deleteRotationUseCase.ts
+++ src/usecase/rotation/deleteRotationUseCase.ts
@@ -1,15 +1,27 @@
 import { BadRequestError } from "@/error/badRequest.error";
+import { UnauthorizedError } from "@/error/unauthorized.error";
 import { RotationRepository } from "@/repository/rotationRepository";
+import { UserRepository } from "@/repository/userRepository";
 
 export class DeleteRotationUseCase {
   constructor(
-    private readonly rotationRepository: RotationRepository
+    private readonly rotationRepository: RotationRepository,
+    private readonly userRepository: UserRepository
   ){}
 
-  async execute(rotationId: string){
+  async execute({userId, rotationId}:{rotationId: string, userId: string}){
     const rotarion = await this.rotationRepository.getById(rotationId)
     if(!rotarion){
       throw new BadRequestError("rotation not exist")
+    }
+
+    const user = await this.userRepository.getById(userId)
+    if(!user){
+      throw new BadRequestError("user not exist")
+    } 
+
+    if(rotarion.createdById !== userId){
+      throw new UnauthorizedError("you not have permission to delete this") 
     }
 
     await this.rotationRepository.delete(rotationId)
