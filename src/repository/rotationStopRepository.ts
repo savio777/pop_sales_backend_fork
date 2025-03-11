c846871b620af5ecf3a366b023975d0a3d948bb6
Gabriel Andrade <138128244+MendoncaGabriel@users.noreply.github.com>
2025-03-10 22:22:34 -0400

    feat: create interface rotation stop to repository

---
+++ src/repository/rotationStopRepository.ts
@@ -0,0 +1,8 @@
+import { Prisma, RotationStop } from "@prisma/client";
+
+export interface RotationStopRepository {
+  create(data: Prisma.RotationStopCreateInput): Promise<RotationStop>
+  getById(id: string): Promise<RotationStop | null>
+  update(data: Prisma.RotationStopUpdateInput): Promise<RotationStop>
+  delete(id: string): Promise<void>
+}
