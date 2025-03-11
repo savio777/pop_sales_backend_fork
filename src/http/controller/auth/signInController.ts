cb4e8b0e4f0a919f959578009b9be95ea52f1265
Gabriel Andrade <138128244+MendoncaGabriel@users.noreply.github.com>
2025-03-11 13:26:15 -0400

    fix: move logic sigin to controler and usecase only get user by email

--- src/http/controller/auth/signInController.ts
+++ src/http/controller/auth/signInController.ts
@@ -1,7 +1,11 @@
 import { PrismaUserRepository } from "@/repository/prisma/prismaUserRepository";
-import { SignInUseCase } from "@/usecase/auth/signInUseCase";
 import { FastifyReply, FastifyRequest } from "fastify";
 import { z } from "zod";
+import bcrypt from 'bcrypt';
+import jwt from "jsonwebtoken"
+import { UnauthorizedError } from "@/error/unauthorized.error";
+import { GetUserByEmailUseCase } from "@/usecase/user/getUserByEmailUseCase";
+import { env } from "@/env";
 
 export class SignInController {
   async handle(req: FastifyRequest, res: FastifyReply){
@@ -9,13 +13,31 @@ export class SignInController {
       email: z.string().email(),
       password: z.string()
     })
-
     const data = signUpSchema.parse(req.body)
 
     const userRepository = new PrismaUserRepository()
-    const signUpUseCase = new SignInUseCase(userRepository)
+    const getUserByEmailUseCase = new GetUserByEmailUseCase(userRepository)
+    const {user} = await getUserByEmailUseCase.execute(data.email)
 
-    const {user, token} = await signUpUseCase.execute(data)
+
+    const isPasswordCorrect = await bcrypt.compare(data.password, user.password);
+    if (!isPasswordCorrect) {
+      throw new UnauthorizedError("e-mail or password are not correct")
+    }
+
+    const payload = {
+      userId: user.id
+    }
+
+    const signOptions = {
+      expiresIn: 60 * 60 * 24 * 7 // 7 days
+    }
+
+    const token = jwt.sign(
+      payload,
+      env.PRIVATE_KEY, 
+      signOptions 
+    );
 
     return res.status(200).send({user, token})
   }
