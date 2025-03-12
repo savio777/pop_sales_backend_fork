3bc3a2725bd75ddb54642b9564cf09c2c97d26b7
Gabriel Andrade <138128244+MendoncaGabriel@users.noreply.github.com>
2025-03-12 13:24:08 -0400

    feat(usecase): delete task

--- src/usecase/task/deleteTaskUseCase.ts
+++ src/usecase/task/deleteTaskUseCase.ts
@@ -1,3 +1,4 @@
+import { NotFoundError } from "@/error/notfound.error";
 import { TaskRepository } from "@/repository/taskRepository";
 
 export class DeleteTaskUseCase {
@@ -6,7 +7,10 @@ export class DeleteTaskUseCase {
   ){}
 
   async execute(taskId: string){
-    const task = await this.taskRepository.
+    const task = await this.taskRepository.getById(taskId)
+    if(!task){
+      throw new NotFoundError("task not found")
+    }
     await this.taskRepository.delete(taskId)
   }
 }
