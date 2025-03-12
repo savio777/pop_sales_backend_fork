813b8ba25bbfa43d8a2e9576b08d4fcf6c8f5d86
Gabriel Andrade <138128244+MendoncaGabriel@users.noreply.github.com>
2025-03-12 13:40:08 -0400

    feat(controller): create task

---
+++ src/http/controller/task/createTaskController.ts
@@ -0,0 +1,39 @@
+import { BadRequestError } from "@/error/badRequest.error";
+import { PrismaCompanyRepository } from "@/repository/prisma/prismaCompanyRepository";
+import { PrismaTaskRepository } from "@/repository/prisma/prismaTaskRepository";
+import { PrismaUserRepository } from "@/repository/prisma/prismaUserRepository";
+import { CreateTaskUseCase } from "@/usecase/task/createTaskUseCase";
+import { FastifyReply, FastifyRequest } from "fastify";
+import { z } from "zod";
+
+export class CreateTaskController {
+  async handle(req: FastifyRequest, res: FastifyReply){
+
+    if (!req.userAuth?.id) {
+      throw new BadRequestError("userId not informed");
+    }
+    const userCreatedId = req.userAuth.id
+
+    const createTaskRequestBody = z.object({
+      companyId: z.string().uuid(),
+      title: z.string(), 
+      description: z.string().optional(), 
+      rotationId: z.string().uuid(), 
+      userAssignedId: z.string().uuid()
+    })
+    const data = createTaskRequestBody.parse(req.body)
+
+    const taskRepository = new PrismaTaskRepository()
+    const companyRepository = new PrismaCompanyRepository()
+    const userRepository = new PrismaUserRepository()
+
+    const createTaskUseCase = new CreateTaskUseCase(
+      taskRepository,
+      companyRepository,
+      userRepository
+    )
+
+    const task = await createTaskUseCase.execute({...data, userCreatedId})
+    return res.status(200).send(task)
+  }
+}
