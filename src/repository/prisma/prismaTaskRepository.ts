16c44ebd533f72e6b01e12e3193ef3eefb60e7ad
Gabriel Andrade <138128244+MendoncaGabriel@users.noreply.github.com>
2025-03-12 15:24:39 -0400

    feat(repository): list tasks

--- src/repository/prisma/prismaTaskRepository.ts
+++ src/repository/prisma/prismaTaskRepository.ts
@@ -2,7 +2,7 @@ import { Prisma, Task } from "@prisma/client";
 import { TaskRepository } from "../taskRepository";
 import { db } from "@/lib/prisma";
 
-type TaskWithRelations = {
+interface GetTaskById {
   id: string;
   title: string;
   description: string | null;
@@ -36,8 +36,122 @@ type TaskWithRelations = {
   };
 };
 
+interface listTask {
+  id: string;
+  title: string;
+  description: string | null;
+  finishedAt: Date | null;
+  status: string;
+  createdAt: Date;
+  updatedAt: Date;
+  company?: {
+    id: string;
+    name: string;
+    status: string;
+  };
+  assignedTo?: {
+    id: string;
+    name: string;
+    status: string;
+  };
+  createdBy?: {
+    id: string;
+    name: string;
+    status: string;
+  };
+  rotation?: {
+    id: string;
+    createdAt: Date;
+    stops: {
+      id: string;
+      address: string;
+      sequence: number;
+    }[];
+  };
+}
+
 
 export class PrismaTaskRepository implements TaskRepository {
+  async listTask({
+    limit,
+    page,
+    companyId,
+    createdAt,
+    finishedAt,
+    status,
+    userAssignedId,
+    userCreatedId
+  }: {
+    limit: number;
+    page: number;
+    userCreatedId?: string;
+    userAssignedId?: string;
+    companyId?: string;
+    status?: "COMPLETED" | "PENDING";
+    createdAt?: Date;
+    finishedAt?: Date;
+  }): Promise<listTask[] | null> {
+    const filters: Prisma.TaskWhereInput = {};
+  
+    if (companyId) filters.companyId = companyId;
+    if (userCreatedId) filters.createdById = userCreatedId;
+    if (userAssignedId) filters.assignedToId = userAssignedId;
+    if (status) filters.status = status;
+    if (createdAt) filters.createdAt = { gte: createdAt };
+    if (finishedAt) filters.finishedAt = { lte: finishedAt };
+  
+    const tasks = await db.task.findMany({
+      where: filters,
+      take: limit,
+      skip: (page - 1) * limit,
+      select: {
+        id: true,
+        title: true,
+        description: true,
+        finishedAt: true,
+        status: true,
+        createdAt: true,
+        updatedAt: true,
+        company: {
+          select: {
+            id: true,
+            name: true,
+            status: true
+          }
+        },
+        assignedTo: {
+          select: {
+            id: true,
+            name: true,
+            status: true
+          }
+        },
+        createdBy: {
+          select: {
+            id: true,
+            name: true,
+            status: true
+          }
+        },
+        rotation: {
+          select: {
+            id: true,
+            createdAt: true,
+            stops: {
+              select: {
+                id: true,
+                address: true,
+                sequence: true
+              }
+            }
+          }
+        }
+      }
+    });
+  
+    return tasks;
+  }
+  
   async update(
     {id, data}:
     {id: string, data: Prisma.TaskUpdateInput}
@@ -50,7 +164,7 @@ export class PrismaTaskRepository implements TaskRepository {
     })
     return task
   }
-  async getById(id: string): Promise<TaskWithRelations | null> {
+  async getById(id: string): Promise<GetTaskById | null> {
     const task = await db.task.findUnique({
       where: { id },
       select: {
