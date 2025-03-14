8fb6fe3cb4d17e08fbe12dc8c1cf9f6326bca279
Gabriel Andrade <138128244+MendoncaGabriel@users.noreply.github.com>
2025-03-14 12:00:26 -0400

    fix(controller): create rotation

--- src/http/controller/rotation/createRotationController.ts
+++ src/http/controller/rotation/createRotationController.ts
@@ -1,45 +1,35 @@
 import { BadRequestError } from "@/error/badRequest.error";
 import { PrismaRotationRepository } from "@/repository/prisma/prismaRotationRepository";
-import { PrismaRotationStopRepository } from "@/repository/prisma/prismaRotationStopRepository";
 import { PrismaUserRepository } from "@/repository/prisma/prismaUserRepository";
 import { CreateRotationUseCase } from "@/usecase/rotation/createRotationUseCase";
 import { FastifyReply, FastifyRequest } from "fastify";
 import { z } from "zod";
 
 export class CreateRotationController {
-  async handle(req: FastifyRequest, res: FastifyReply){
-    const stopsRequestBody = z.object({
-      sequence: z.coerce.number(),
-      address: z.string()
-    })
-
+  async handle(req: FastifyRequest, res: FastifyReply) {
     const assignedRequestBody = z.object({
       assignedToId: z.string().uuid(),
-      stops: z.array(stopsRequestBody),
-      tasks: z.array(z.string().uuid())
-    })
+    });
 
-    const {assignedToId, stops, tasks} = assignedRequestBody.parse(req.body)
+    const { assignedToId } = assignedRequestBody.parse(req.body);
 
-    const rotationRepository = new PrismaRotationRepository()
-    const userRepository = new PrismaUserRepository()
-    const rotationStopRepository = new PrismaRotationStopRepository()
+    const rotationRepository = new PrismaRotationRepository();
+    const userRepository = new PrismaUserRepository();
     const createRotationUseCase = new CreateRotationUseCase(
       rotationRepository,
-      rotationStopRepository,
-      userRepository,
-    )
+      userRepository
+    );
 
     if (!req.userAuth?.id) {
       throw new BadRequestError("userId not informed");
     }
-    const createdById = req.userAuth.id
-
+    const createdById = req.userAuth.id;
 
     const rotation = await createRotationUseCase.execute({
-      assignedToId, createdById, stops, tasks
-    })
+      assignedToId,
+      createdById,
+    });
 
-    return res.status(200).send(rotation)
+    return res.status(200).send(rotation);
   }
 }
