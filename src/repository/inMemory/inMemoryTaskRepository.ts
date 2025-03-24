2c8ccb96fed855ab798d50f51b1c9250e95355ec
Gabriel Andrade <138128244+MendoncaGabriel@users.noreply.github.com>
2025-03-24 18:25:28 -0400

    feat: InMemroyTaskRepository

--- src/repository/inMemory/inMemoryTaskRepository.ts
+++ src/repository/inMemory/inMemoryTaskRepository.ts
@@ -5,9 +5,28 @@ import { randomUUID } from "crypto";
 export class InMemroyTaskRepository implements TaskRepository {
   private task: Task[] = []
 
-  update({ id, data }: { id: string; data: Prisma.TaskUpdateInput; }): Promise<Task> {
-    throw new Error("Method not implemented.");
+  async update({ id, data }: { id: string; data: Prisma.TaskUpdateInput }): Promise<Task> {
+    const index = this.task.findIndex(e => e.id === id);
+    
+    if (index === -1) {
+      throw new Error("Task not found.");
+    }
+  
+    const updatedTask: Task = {
+      ...this.task[index],
+      updatedAt: new Date(),
+      description: typeof data.description === "string" ? data.description : this.task[index].description,
+      title: typeof data.title === "string" ? data.title : this.task[index].title,
+      status: typeof data.status === "string" ? data.status : this.task[index].status,
+      finishedAt: data.finishedAt 
+      ? new Date(data.finishedAt as string)
+      : null    };
+  
+    this.task[index] = updatedTask;
+    
+    return updatedTask
   }
+  
   async create(data: Prisma.TaskCreateInput): Promise<Task> {
     const task: Task = {
       id: randomUUID(),
@@ -22,14 +41,23 @@ export class InMemroyTaskRepository implements TaskRepository {
     this.task.push(task)
     return task
   }
-  delete(id: string): Promise<void> {
-    throw new Error("Method not implemented.");
+  async delete(id: string): Promise<void> {
+    const index = this.task.findIndex(e => e.id === id)
+    if(index === -1){
+      throw new Error("task not found")
+    }
+    this.task.slice(index, 1)
   }
-  listByStopId({ limit, page, stopId }: { stopId: string; page: number; limit: number; }): Promise<Task[] | null> {
-    throw new Error("Method not implemented.");
+  async listByStopId({ limit, page, stopId }: { stopId: string; page: number; limit: number; }): Promise<Task[] | null> {
+    const startIndex = (page -1) * limit
+    const endIndex = startIndex + limit
+    
+    const tasks = this.task.filter(e => e.stopId === stopId)
+    return tasks.slice(startIndex, endIndex)
   }
-  getById(id: string): Promise<Task | null> {
-    throw new Error("Method not implemented.");
+  async getById(id: string): Promise<Task | null> {
+    const task = this.task.find(e => e.id === id)
+    return task || null
   }
   
 }
