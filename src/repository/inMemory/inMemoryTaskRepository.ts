e042489cda90950aa38d37c017d844a725f87be2
Gabriel Andrade <138128244+MendoncaGabriel@users.noreply.github.com>
2025-03-24 18:10:00 -0400

    test: create test

--- src/repository/inMemory/inMemoryTaskRepository.ts
+++ src/repository/inMemory/inMemoryTaskRepository.ts
@@ -1,12 +1,26 @@
 import { Prisma, Task } from "@prisma/client";
 import { TaskRepository } from "../taskRepository";
+import { randomUUID } from "crypto";
 
 export class InMemroyTaskRepository implements TaskRepository {
+  private task: Task[] = []
+
   update({ id, data }: { id: string; data: Prisma.TaskUpdateInput; }): Promise<Task> {
     throw new Error("Method not implemented.");
   }
-  create(data: Prisma.TaskCreateInput): Promise<Task> {
-    throw new Error("Method not implemented.");
+  async create(data: Prisma.TaskCreateInput): Promise<Task> {
+    const task: Task = {
+      id: randomUUID(),
+      createdAt: new Date(),
+      description: data.description ?? null,
+      finishedAt: null,
+      status: "PENDING",
+      stopId: (data as any).Stop?.connect?.id,
+      title: data.title,
+      updatedAt: new Date()
+    }
+    this.task.push(task)
+    return task
   }
   delete(id: string): Promise<void> {
     throw new Error("Method not implemented.");
--- src/usecase/task/createTaskUseCase.ts
+++ src/usecase/task/createTaskUseCase.ts
@@ -1,3 +1,5 @@
+import { BadRequestError } from "@/error/badRequest.error";
+import { StopRepository } from "@/repository/stopRepository";
 import { TaskRepository } from "@/repository/taskRepository";
 
 interface CreateTaskSchema {
@@ -9,11 +11,16 @@ interface CreateTaskSchema {
 export class CreateTaskUseCase {
   constructor(
     private readonly taskRepository: TaskRepository,
+    private readonly stopRepository: StopRepository
   ){}
 
   async execute(
     {title, description, stopId}:CreateTaskSchema
   ){    
+    const stop = await this.stopRepository.getById(stopId)
+    if(!stop){
+      throw new BadRequestError("stop not fould")
+    }
 
     const task = await this.taskRepository.create({
       title,
