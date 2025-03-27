982f870539e24e302474c497a12f902a364de028
Gabriel Andrade <138128244+MendoncaGabriel@users.noreply.github.com>
2025-03-27 14:28:12 -0400

    feat(repository): get by name and get by email

--- src/repository/clientRepository.ts
+++ src/repository/clientRepository.ts
@@ -2,4 +2,6 @@ import { Client, Prisma } from "@prisma/client";
 
 export interface ClientRepository {
   create(data: Prisma.ClientCreateInput): Promise<Client>
+  getByName(name: string): Promise<Client | null>
+  getByEmail(email: string): Promise<Client | null>
 }
--- src/repository/prisma/prismaClientRepository.ts
+++ src/repository/prisma/prismaClientRepository.ts
@@ -3,6 +3,22 @@ import { ClientRepository } from "../clientRepository";
 import { db } from "@/lib/prisma";
 
 export class PrismaClientRepository implements ClientRepository {
+  async getByName(name: string): Promise<Client | null> {
+    const client = await db.client.findFirst({
+      where: {
+        name
+      }
+    })
+    return client
+  }
+  async getByEmail(email: string): Promise<Client | null> {
+    const client = await db.client.findFirst({
+      where: {
+        email
+      }
+    })
+    return client
+  }
   async create(data: Prisma.ClientCreateInput): Promise<Client> {
     const client = await db.client.create({
       data
