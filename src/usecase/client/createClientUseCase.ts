2a4d45b15ff8c0a7f070d0e118e2858ec074cd82
Gabriel Andrade <138128244+MendoncaGabriel@users.noreply.github.com>
2025-03-27 13:54:38 -0400

    feat: create client usecase

---
+++ src/usecase/client/createClientUseCase.ts
@@ -0,0 +1,50 @@
+import { BadRequestError } from "@/error/badRequest.error";
+import { ClientRepository } from "@/repository/clientRepository";
+import { CompanyRepository } from "@/repository/companyRepository";
+
+export class CreateClientUseCase {
+  constructor(
+    private readonly clientRepository: ClientRepository,
+    private readonly companyRepository: CompanyRepository
+  ){}
+
+  async execute(
+    {
+      name,
+      email,
+      lat,
+      lon,
+      phoneNumber,
+      zipCode,
+      responsiblePerson,
+      companyId
+    }: {
+      name: string,
+      email?: string,
+      lon?: string,
+      lat?: string,
+      phoneNumber?: string,
+      zipCode?: string,
+      responsiblePerson?: string,
+      companyId: string
+    }
+  ){
+    const company = await this.companyRepository.getById(companyId)
+    if(!company){
+      throw new BadRequestError("company is not exist")
+    }
+    
+    const client = await this.clientRepository.create({
+      name, 
+      email, 
+      lon, 
+      lat, 
+      phoneNumber, 
+      zipCode, 
+      Company: {connect: {id: companyId}},
+      responsiblePerson
+    })
+
+    return {client}
+  }
+}
