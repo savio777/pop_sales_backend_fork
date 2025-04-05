d31f3784b5796d5b27794df2e1433d5ca7bce81a
Gabriel Andrade <138128244+MendoncaGabriel@users.noreply.github.com>
2025-04-05 19:13:53 -0400

    fix: list client service impleent method listClientService

--- src/http/controller/client/listClientServiceController.ts
+++ src/http/controller/client/listClientServiceController.ts
@@ -1,3 +1,4 @@
+import { PrismaClientRepository } from "@/repository/prisma/prismaClientRepository";
 import { PrismaCompanyRepository } from "@/repository/prisma/prismaCompanyRepository";
 import { ListClientServiceUseCase } from "@/usecase/client/ListClientServiceUseCase";
 import { FastifyReply, FastifyRequest } from "fastify";
@@ -11,8 +12,11 @@ export class ListClientServiceController {
     const { companyId } = listClientServiceRequestParams.parse(req.params);
 
     const companyRepository = new PrismaCompanyRepository()
+    const clientRepository = new PrismaClientRepository()
+    
     const listClientServiceUseCase = new ListClientServiceUseCase( 
-      companyRepository
+      companyRepository,
+      clientRepository
     );
 
     const clientService = await listClientServiceUseCase.execute({
--- src/usecase/client/ListClientServiceUseCase.ts
+++ src/usecase/client/ListClientServiceUseCase.ts
@@ -1,12 +1,14 @@
 import { NotFoundError } from "@/error/notfound.error";
 import { db } from "@/lib/prisma";
+import { ClientRepository } from "@/repository/clientRepository";
 import { CompanyRepository } from "@/repository/companyRepository";
 
 /* deve listar clientes de uma empresa que estão em atendimento */
 
 export class ListClientServiceUseCase {
   constructor(
-    private readonly companyRepository: CompanyRepository
+    private readonly companyRepository: CompanyRepository,
+    private readonly clientRepository: ClientRepository
   ){}
 
   async execute(
@@ -18,17 +20,23 @@ export class ListClientServiceUseCase {
       throw new NotFoundError("Empresa não encontrado")
     }
 
-    const clientService = await db.checkinCheckout.findMany({
-      where: {
-        finalizedAt: null,
-        client: {
-          companyId: company.id
-        }
-      },
-      select: {
-        client: true
-      }
-    })
+    const clientService =  await this.clientRepository.listClientService(companyId)
+    if(!clientService){
+      throw new NotFoundError("Nenhum cliente em atendimento")
+    }
+
+    //TODO: apagar, removido para implementar o novo metodo list client service
+    // const clientService = await db.checkinCheckout.findMany({
+    //   where: {
+    //     finalizedAt: null,
+    //     client: {
+    //       companyId: company.id
+    //     }
+    //   },
+    //   select: {
+    //     client: true
+    //   }
+    // })
 
     return {clientService}
   }
