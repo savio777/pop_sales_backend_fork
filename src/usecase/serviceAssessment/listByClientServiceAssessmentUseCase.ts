03f2f41c0509936266e4f15de1ab37b2c48b9540
Gabriel Andrade <138128244+MendoncaGabriel@users.noreply.github.com>
2025-04-10 17:07:26 -0400

    feat(usecase): listByClientServiceAssessmentUseCase

---
+++ src/usecase/serviceAssessment/listByClientServiceAssessmentUseCase.ts
@@ -0,0 +1,23 @@
+import { BadRequestError } from "@/error/badRequest.error";
+import { NotFoundError } from "@/error/notfound.error";
+import { ClientRepository } from "@/repository/clientRepository";
+import { ServiceAssessmentRepository } from "@/repository/serviceAssessmentRepository";
+
+export class ListByClientServiceAssessmentUseCase {
+  constructor(
+    private readonly serviceAssessmentRepository: ServiceAssessmentRepository,
+    private readonly clientRepository: ClientRepository,
+  ) {}
+
+  async execute(clientId: string) {
+
+    const client = await this.clientRepository.getById(clientId);
+    if(!client) {
+      throw new NotFoundError("Cliente não encontrado")
+    }
+
+    const serviceAssessment = await this.serviceAssessmentRepository.listByClient(clientId);
+
+    return {serviceAssessment}
+  }
+}
