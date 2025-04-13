31aad7c899c8734ec70b04d7c1f234eb9e1c39b8
Gabriel Andrade <138128244+MendoncaGabriel@users.noreply.github.com>
2025-04-13 17:59:20 -0400

    feat(controller): CreateFormController

---
+++ src/http/controller/form/createFormController.ts
@@ -0,0 +1,37 @@
+import { PrismaCompanyRepository } from "@/repository/prisma/prismaCompanyRepository";
+import { PrismaFormRepository } from "@/repository/prisma/prismaFormRepository";
+import { CreateFormUseCase } from "@/usecase/form/createFormUseCase";
+import { FastifyReply, FastifyRequest } from "fastify";
+import { z } from "zod";
+
+export class CreateFormController {
+  async handle(req: FastifyRequest, res: FastifyReply){
+
+    const createFormRequestBody = z.object({
+      companyId: z.string(),
+      formType: z.enum(["CLIENT", "PROMOTER", "SELLER","DELIVERER"]),
+      questions: z.array(
+        z.object({
+          text: z.string(),
+          type: z.enum(["TEXT", "YES_NO", "MULTIPLE_CHOICE", "IMAGE"]),
+          required: z.boolean(),
+        })
+      )
+    })
+
+    const { companyId, formType, questions } = createFormRequestBody.parse(req.body)
+
+    const formRepository = new PrismaFormRepository() 
+    const companyRepository = new PrismaCompanyRepository()
+    const createForm = new CreateFormUseCase(
+      formRepository,
+      companyRepository
+    );
+
+    const form = await createForm.execute({
+      companyId, formType, questions
+    })
+
+    return res.status(201).send(form)
+  }
+}
