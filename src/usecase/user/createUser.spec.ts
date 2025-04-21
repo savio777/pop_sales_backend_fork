a47c964bfb240b3ebb199c41e35ab2397c0b03b5
Gabriel Andrade <138128244+MendoncaGabriel@users.noreply.github.com>
2025-04-21 08:15:20 -0400

    test: create user usecase

--- src/usecase/user/createUser.spec.ts
+++ src/usecase/user/createUser.spec.ts
@@ -1,8 +1,46 @@
-import { describe } from "node:test";
+import { mockCompany } from "@/test/mocks/mockCompany";
+import { CreateUserUseCase } from "./createUserUseCase";
+import { mockUser } from "@/test/mocks/mockUser";
+import { mockUserCompany } from "@/test/mocks/mockUserCompany";
+
+const mockUserRepository = {
+  create: jest.fn(),
+  getByEmail: jest.fn(),
+};
+
+const mockCompanyRepository = {
+  getById: jest.fn(),
+};
+
+const mockUserCompanyRepository = {
+  create: jest.fn(),
+};
 
 describe("Create User", () => {
-  it("should create a new user", () => {
-    console.log("Hello World")
-  })
+  let sut: CreateUserUseCase;
 
-})
+  beforeEach(() => {
+    sut = new CreateUserUseCase(
+      mockUserRepository as any,
+      mockCompanyRepository as any,
+      mockUserCompanyRepository as any
+    );
+    jest.clearAllMocks();
+  });
+
+  it("should create a new user", async () => {
+    mockCompanyRepository.getById.mockResolvedValue(mockCompany);
+    mockUserRepository.getByEmail.mockResolvedValue(null);
+    mockUserRepository.create.mockResolvedValue(mockUser);
+    mockUserCompanyRepository.create.mockResolvedValue(mockUserCompany);
+
+    const { user } = await sut.execute({
+      name: mockUser.name,
+      email: mockUser.email,
+      password: mockUser.password,
+      companyId: mockCompany.id,
+    });
+
+    expect(user).toEqual(mockUser);
+  });
+});
