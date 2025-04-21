604d6161c18f899c8fb19ce1704a1ce2ad35d376
Gabriel Andrade <138128244+MendoncaGabriel@users.noreply.github.com>
2025-04-21 08:37:06 -0400

    test: get user by id use case

--- src/usecase/user/getUserById.spec.ts
+++ src/usecase/user/getUserById.spec.ts
@@ -1,7 +1,22 @@
+import { mockUserRepository } from "@/test/mocks/mockRepositoryJest";
 import { GetUserByIdUseCase } from "./getUserByIdUseCase";
+import { mockUser } from "@/test/mocks/mockEntities";
 
 describe("Get User By Id", () => {
   let sut: GetUserByIdUseCase;
-  
 
+  beforeEach(() => {
+    sut = new GetUserByIdUseCase(
+      mockUserRepository as any,
+    );
+    jest.clearAllMocks();
+  });
+
+  it("should get a user by id", async () => {
+    mockUserRepository.getById.mockResolvedValue(mockUser);
+
+    const { user } = await sut.execute(mockUser.id);
+
+    expect(user).toEqual(mockUser);
+  });
 })
