86fa7f249075d176d624253d998b1e5b9c97eb21
Gabriel Andrade <138128244+MendoncaGabriel@users.noreply.github.com>
2025-04-23 18:52:30 -0400

    test: delete company usecase

--- src/usecase/company/deleteCompanyUseCase.ts
+++ src/usecase/company/deleteCompanyUseCase.ts
@@ -1,17 +1,16 @@
-import { BadRequestError } from "@/error/badRequest.error";
+import { NotFoundError } from "@/error/notfound.error";
 import { CompanyRepository } from "@/repository/companyRepository";
 
-export class DeleteCompanyUdeCase {
-  constructor(
-    private readonly companyRepositoy: CompanyRepository
-  ){}
+export class DeleteCompanyUseCase {
+  constructor(private readonly companyRepositoy: CompanyRepository) {}
 
-  async execute(id: string){
-    const comapany = await this.companyRepositoy.getById(id)
-    if(!comapany){
-      throw new BadRequestError("Empresa não existe.")
+  async execute(id: string) {
+    const comapanyExist = await this.companyRepositoy.getById(id);
+    if (!comapanyExist) {
+      throw new NotFoundError("Empresa não existe.");
     }
-    
-    await this.companyRepositoy.delete(id)
+
+    const company = await this.companyRepositoy.delete(id);
+    return { company };
   }
 }
