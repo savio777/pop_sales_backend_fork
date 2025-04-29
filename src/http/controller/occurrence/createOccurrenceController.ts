bef12ddcd21671ea77853c8d0c88f9aade11b1a1
Gabriel Andrade <138128244+MendoncaGabriel@users.noreply.github.com>
2025-04-29 19:21:17 -0400

    fix: lint controller

--- src/http/controller/occurrence/createOccurrenceController.ts
+++ src/http/controller/occurrence/createOccurrenceController.ts
@@ -5,26 +5,29 @@ import { connect } from "http2";
 import { z } from "zod";
 
 export class CreateOccurrenceController {
-  async handler(req: FastifyRequest, res: FastifyReply){
-
+  async handler(req: FastifyRequest, res: FastifyReply) {
     const requestBody = z.object({
       userId: z.string().uuid(),
       title: z.string(),
       description: z.string(),
-      companyId: z.string().uuid()
-    })
-    const {companyId, description, title, userId} = requestBody.parse(req.body)
+      companyId: z.string().uuid(),
+    });
+    const { companyId, description, title, userId } = requestBody.parse(
+      req.body
+    );
 
-    const prismaOccurrenceRepsitory = new PrismaOccurrenceRepository()
-    const createOccurrenceUseCase = new CreateOcurrenceUseCase(prismaOccurrenceRepsitory)
+    const prismaOccurrenceRepsitory = new PrismaOccurrenceRepository();
+    const createOccurrenceUseCase = new CreateOcurrenceUseCase(
+      prismaOccurrenceRepsitory
+    );
 
     const occurrence = await createOccurrenceUseCase.execute({
-      Company: {connect: {id: companyId}},
-      User: {connect: {id: userId}},
-      title, 
-      description
-    })
+      Company: { connect: { id: companyId } },
+      User: { connect: { id: userId } },
+      title,
+      description,
+    });
 
-    return res.status(201).send(occurrence)
+    return res.status(201).send(occurrence);
   }
 }
