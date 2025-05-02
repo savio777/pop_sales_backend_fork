252757ca088680293fc5f69c40e9442f6f604ac5
Gabriel Andrade <138128244+MendoncaGabriel@users.noreply.github.com>
2025-05-02 12:58:00 -0400

    feat: change password controller add routes

--- src/http/routes/authRoute.ts
+++ src/http/routes/authRoute.ts
@@ -1,11 +1,14 @@
 import { FastifyInstance } from "fastify";
 import { SignInController } from "../controller/auth/signInController";
 import { SignUpController } from "../controller/auth/signUpController";
+import { ChangePasswordController } from "../controller/auth/ChangePasswordController";
 
-const signInController = new SignInController()
-const signUpController = new SignUpController()
+const signInController = new SignInController();
+const signUpController = new SignUpController();
+const changePasswordController = new ChangePasswordController();
 
-export function AuthRoutes(app: FastifyInstance){
-  app.post("/sign-in", signInController.handle)
-  app.post("/sign-up", signUpController.handle)
+export function AuthRoutes(app: FastifyInstance) {
+  app.post("/sign-in", signInController.handle);
+  app.post("/sign-up", signUpController.handle);
+  app.post("/change-password", changePasswordController.handler);
 } 
