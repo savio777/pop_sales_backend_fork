f699aaaac5d64fa724c7e91cdeb2467e814e3a83
savio <saviopf22@gmail.com>
2025-07-13 20:20:21 -0300

    add type user filter

--- src/http/controller/user/listUserByCompanyIdController.ts
+++ src/http/controller/user/listUserByCompanyIdController.ts
@@ -4,40 +4,43 @@ import { FastifyReply, FastifyRequest } from "fastify";
 import { z } from "zod";
 
 export class ListUserByCompanyIdController {
-  async handler(req: FastifyRequest, res: FastifyReply){
+  async handler(req: FastifyRequest, res: FastifyReply) {
     const requestParams = z.object({
       companyId: z.string().uuid(),
-    })
+    });
 
     const requestQuery = z.object({
       page: z.coerce.number().min(1).default(1),
       limit: z.coerce.number().min(1).default(100),
-    })
+      type: z.enum(["EMPLOYEE", "MANAGER"]).optional(),
+    });
 
-    const { companyId } = requestParams.parse(req.params)
-    const { page, limit } = requestQuery.parse(req.query)
+    const { companyId } = requestParams.parse(req.params);
+    const { page, limit, type } = requestQuery.parse(req.query);
 
     const company = await db.company.findUnique({
       where: {
         id: companyId,
       },
-    })
+    });
 
-    if(!company){
-      throw new NotFoundError("Empresa não encontrada")
+    if (!company) {
+      throw new NotFoundError("Empresa não encontrada");
     }
 
-    const offset = (page - 1) * limit
+    const offset = (page - 1) * limit;
     const result = await db.userCompany.findMany({
       where: {
         companyId,
-        User: {
-          name: {
-            not: {
-              equals: "admin"
+        User: !type
+          ? {
+              type: {
+                not: {
+                  equals: "ADMIN",
+                },
+              },
             }
-          }
-        }
+          : { type },
       },
       select: {
         User: {
@@ -47,19 +50,20 @@ export class ListUserByCompanyIdController {
             email: true,
             createdAt: true,
             updatedAt: true,
-            status: true, 
+            status: true,
             phone: true,
-            type: true
-          }
-        }
+            type: true,
+          },
+        },
       },
       take: limit,
       skip: offset,
-    })
-    
+    });
 
-    const users = result.map(item => item.User).filter((user): user is NonNullable<typeof user> => user !== null)
+    const users = result
+      .map((item) => item.User)
+      .filter((user): user is NonNullable<typeof user> => user !== null);
 
-    return res.status(200).send({users})
+    return res.status(200).send({ users });
   }
 }
